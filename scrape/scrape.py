8a81fa7a564ce4196eba62a5bb9fb2a8d2bbd583
Gabriel Simoes Gaspar <gabrielsgaspar@gmail.com>
2020-10-02 16:01:25 +0100

    Set up scrapper

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -16,36 +16,37 @@ import numpy as np
 from flatten_json import flatten
 from tqdm.auto import tqdm
 
+# Define function to check if data folders exist
+def data_folder(museums):
+    """
+    This function verifies if a folder exists for each museum in the argument as key
+    and creates one in case folder is missing.
+
+    Argument:
+        museums: a dictionary with museum names as keys and a tuple containing the
+                 address as the first argument and Tainacam collection as the second
+    Output:
+        ../data/[MUSEM]: a folder for each museum in argument
+
+    """
+    # Set loop to check if folder exists for each museum
+    for museum in museums:
+        if not os.path.exists('../data/{}'.format(museum)):
+            os.makedirs('../data/{}'.format(museum))
+
 # Define function to download all museum data
-def download_museum():
+def download_museum(museums):
     """
     This function downloads all the data available for the museums in the Ibram
     website that use Tainacam as a platform to host their APIs.
 
     Argument:
-        None
+        museums: a dictionary with museum names as keys and a tuple containing the
+                 address as the first argument and Tainacam collection as the second
     Output:
-        None
+        museum_data: a .csv file with all the available data for the museums
 
     """
-    # Set up url handle of apis for museums that use Tainacam as dictionary
-    museums = {
-                "Museu_Historico_Nacional": ("http://mhn.acervos.museus.gov.br", 24),
-                "Museu_do_Diamante": ("https://museudodiamante.acervos.museus.gov.br", 11603),
-                "Museu_do_Ouro": ("https://museudoouro.acervos.museus.gov.br", 3583),
-                "Museu_Victor_Meirelles": ("http://museuvictormeirelles.acervos.museus.gov.br", 18004),
-                "Museu_Regional_Casa_dos_Ottoni": ("https://museuregionalcasadosottoni.acervos.museus.gov.br", 31492),
-                "Museu_de_Arqueologia_de_Itaipu": ("http://museudearqueologiadeitaipu.museus.gov.br", 94553),
-                "Museu_das_Missoes": ("http://museudasmissoes.acervos.museus.gov.br", 6409),
-                "Museu_da_Bandeiras": ("http://museusibramgoias.acervos.museus.gov.br", 1093),
-                "Museu_Villa_Lobos": ("http://museuvillalobos.acervos.museus.gov.br", 1570),
-                "Museu_Casa_de_Benjamin_Constant": ("http://museucasabenjaminconstant.acervos.museus.gov.br", 5347),
-                "Museu_da_Inconfidencia": ("http://museudainconfidencia.acervos.museus.gov.br", 9),
-                "Museu_Regional_de_Sao_Joao_del-Rei": ("http://museuregionaldesaojoaodelrei.acervos.museus.gov.br", 11451),
-                "Museu_de_Arte_Sacra_da_Boa_Morte": ("http://museusibramgoias.acervos.museus.gov.br", 3387),
-                "Museu_Casa_da_Hera": ("http://museucasadahera.acervos.museus.gov.br", 7),
-                "Museu_Casa_Histórica_de_Alcantara": ("http://museucasahistoricadealcantara.acervos.museus.gov.br/", 14284)
-            }
     # Loop through museums and retrieve all available data
     for museum in tqdm(museums):
         # Set initial value for page and dataframe to hold museum data
@@ -71,7 +72,7 @@ def download_museum():
 
 if __name__ == "__main__":
     # Print message for download
-    print("\nWelcome to the Brazilian Museum scraper!\n")
+    print("Welcome to the Brazilian Museum scraper!\n")
     print("""\
                  ( (
                   ) )
@@ -79,9 +80,31 @@ if __name__ == "__main__":
                  \__/-'
                 `----'  @gabrielsgaspar
         """)
-    print("Downloading data for Brazilian museums via Tainacam")
+    # Set up url handle of apis for museums that use Tainacam as dictionary
+    museums = {
+                "Museu_Historico_Nacional": ("http://mhn.acervos.museus.gov.br", 24),
+                "Museu_do_Diamante": ("https://museudodiamante.acervos.museus.gov.br", 11603),
+                "Museu_do_Ouro": ("https://museudoouro.acervos.museus.gov.br", 3583),
+                "Museu_Victor_Meirelles": ("http://museuvictormeirelles.acervos.museus.gov.br", 18004),
+                "Museu_Regional_Casa_dos_Ottoni": ("https://museuregionalcasadosottoni.acervos.museus.gov.br", 31492),
+                "Museu_de_Arqueologia_de_Itaipu": ("http://museudearqueologiadeitaipu.museus.gov.br", 94553),
+                "Museu_das_Missoes": ("http://museudasmissoes.acervos.museus.gov.br", 6409),
+                "Museu_da_Bandeiras": ("http://museusibramgoias.acervos.museus.gov.br", 1093),
+                "Museu_Villa_Lobos": ("http://museuvillalobos.acervos.museus.gov.br", 1570),
+                "Museu_Casa_de_Benjamin_Constant": ("http://museucasabenjaminconstant.acervos.museus.gov.br", 5347),
+                "Museu_da_Inconfidencia": ("http://museudainconfidencia.acervos.museus.gov.br", 9),
+                "Museu_Regional_de_Sao_Joao_del-Rei": ("http://museuregionaldesaojoaodelrei.acervos.museus.gov.br", 11451),
+                "Museu_de_Arte_Sacra_da_Boa_Morte": ("http://museusibramgoias.acervos.museus.gov.br", 3387),
+                "Museu_Casa_da_Hera": ("http://museucasadahera.acervos.museus.gov.br", 7),
+                "Museu_Casa_Histórica_de_Alcantara": ("http://museucasahistoricadealcantara.acervos.museus.gov.br/", 14284)
+            }
+    # Check if data directory exists for each museum
+    print("Checking if data directories exist ...")
+    data_folder(museums)
     time.sleep(2)
-    print("Downloading data ...")
     # Call function to download data
-    download_museum()
+    print("Downloading data ...")
+    download_museum(museums)
+    time.sleep(2)
+    # Print complete message and exit
     print("Download of Brazilian museums done!")
